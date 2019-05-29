0b19f65a8bab7708579ad0baa6a121d436179dd4
Dominic Doty <ddoty@server.dominicdoty@gmail.com>
2019-05-29 10:44:57 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # sshd_daily_update
 A simple script that parses the auth &amp; fail2ban logs for emailing to an admin every morning.
+
+It provides analysis of the previous day's traffic, with output like this:
+
+```
+Analyzing logs from May 28 00:00:01 to May 28 23:59:59
+
+There were 2 successful login(s) from 1 account(s) and 1 IP address(es)
+The top username(s) were:
+      2 realusername
+The top IP(s) were:
+      2 xxx.xxx.xxx.xxx  :  US, United States        
+
+There were 63 failed login(s) from 1 account(s) and 55 IP address(es)
+The top username(s) were:
+      7 admin
+      4 www
+      4 user
+      3 ftpuser
+      2 zabbix
+The top IP(s) were:
+      6 185.254.122.114  :  IP Address not found     BANNED
+      2 41.231.56.98     :  TN, Tunisia              BANNED
+      2 167.99.8.158     :  US, United States        
+      2 104.196.16.112   :  US, United States        
+      1 91.122.14.178    :  RU, Russian Federation   
+
+Fail2Ban blocked 2 IP address(es) that attempted to connect too much
+```
+
+## Setup
+* The script assumes your auth logs are at /var/log/ and you have auth.log, auth.log.1, fail2ban.log, and fail2ban.log.1
+* You must have geoiplookup installed (updating the database with geoipupdate is probably also smart)
+* You must have fail2ban installed and running (or the fail2ban part won't work)
+* Simple method is to call the script from the cmdline for a report printed to stdout
+* Or use the included cron script (place in /etc/cron.daily/), configure the script, and configure sendmail for sending email
+* There are comments in the cron script explaining what needs to be configured
+
+## Thanks
+* SMTP Provided by Mailgun  [mailgun](https://www.mailgun.com/)
+* IP Lookup with GeoLite2 from MaxMind
+> This product includes GeoLite2 data created by MaxMind, available from [maxmind.com](https://www.maxmind.com)
---
+++ sshd_daily_update.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+cd /tmp
+
+##ARGUMENT HANDLING
+while getopts 'me:' flag; do
+  case "${flag}" in
+    m) SENDMAILFLAG='true' ;;
+    e) EMAIL="${OPTARG}" ;;
+    *) SENDMAILFLAG='false';;
+  esac
+done
+
+##HTML HEADER JUNK
+if [ "$SENDMAILFLAG" = "true" ]; then
+	echo "From: $USER@$HOSTNAME"
+	echo "To: $EMAIL"
+	echo "Subject: Daily Log Analysis"
+	echo "MIME-Version: 1.0"
+	echo "Content-Type: text/html"
+	echo "Content-Disposition: inline"
+	echo "<html>"
+	echo "<body>"
+	echo "<pre style=\"font: monospace\">"
+fi
+
+# Create a Logfile with the last day
+grep "`date --date='1 days ago' +"%b %e"`" /var/log/auth.log.1 > day.log
+grep "`date --date='1 days ago' +"%b %e"`" /var/log/auth.log >> day.log
+
+# How many hours the logs cover
+echo -n "Analyzing logs from "
+head -n 1 day.log | grep -P -o "\S+ \w+ (\d{2}:){2}\d{2}" | tr -d '\n' # Start
+echo -n " to "
+tail -n 1 day.log | grep -P -o "\S+ \w+ (\d{2}:){2}\d{2}" # End
+echo ""
+
+##SUCCESS SECTION
+# Grab successful logins and put them in a file
+grep "Accepted" day.log > successful_auths.log
+
+# How many successful logins there were
+successful_auths_count=$(wc -l < successful_auths.log | tr -d '\n')
+
+# How many successful unique usernames there were
+successful_users_count=$(cut -d " " -f 9 successful_auths.log | sort | uniq | wc -l | tr -d '\n')
+
+# How many successful unique IPs there were
+successful_ips_count=$(cut -d " " -f 11 successful_auths.log | sort | uniq | wc -l | tr -d '\n')
+
+# Print words
+printf "There were %d successful login(s) from %d account(s) and %d IP address(es)\n" "$successful_auths_count" "$successful_users_count" "$successful_ips_count"
+
+# Skip printing "Top users" if nobody logged in
+if [ "$successful_auths_count" != "0" ]; then
+
+	# What were the top successful usernames
+	echo "The top username(s) were:"
+	cut -d " " -f 9 successful_auths.log | sort | uniq -c | sort -nr | head -n 5
+
+	# What were the top successful IPs
+	echo "The top IP(s) were:"
+	cut -d " " -f 11 successful_auths.log | sort | uniq -c | sort -nr | head -n 5 > successful_ips.log
+
+	while read line; do
+		location=$(echo "$line" | cut -d " " -f 2 | xargs geoiplookup | cut -d: -f 2)
+		printf "      %-18s : %-25s %s\n" "$line" "$location"
+	done < successful_ips.log
+
+	echo ""
+else
+	echo ""
+fi
+
+
+##FAIL2BAN PREP
+# Create a Logfile with the last day
+grep "`date --date='1 days ago' +"%F"`" /var/log/fail2ban.log.1 | grep Ban > fail2ban_day.log
+grep "`date --date='1 days ago' +"%F"`" /var/log/fail2ban.log | grep Ban >> fail2ban_day.log
+
+##FAILURE SECTION
+# Grab failed logins and put them in a file
+#grep "Invalid" day.log | awk -F 'user' '{print $2}' > failed_auths.log
+grep "Invalid" day.log > failed_auths.log
+
+# How many failed logins there were
+failed_auths_count=$(wc -l < failed_auths.log | tr -d '\n')
+
+# How many failed unique usernames there were
+failed_users_count=$(cut -d " " -f 11 failed_auths.log | sort | uniq | wc -l | tr -d '\n')
+
+# How many failed unique IPs there were
+failed_ips_count=$(cut -d " " -f 10 failed_auths.log | sort | uniq | wc -l | tr -d '\n')
+
+# Print words
+printf "There were %d failed login(s) from %d account(s) and %d IP address(es)\n" "$failed_auths_count" "$failed_users_count" "$failed_ips_count"
+
+
+# Skip printing "Top users" if nobody logged in
+if [ "$failed_auths_count" != "0" ]; then
+
+	# What were the top failed usernames
+	echo "The top username(s) were:"
+	cut -d " " -f 8 failed_auths.log | sort | uniq -c | sort -nr | head -n 5
+
+	# What were the top failed IPs
+	echo "The top IP(s) were:"
+	cut -d " " -f 10 failed_auths.log | sort | uniq -c | sort -nr | head -n 5 > failed_ips.log
+
+	while read line; do
+	        location=$(echo "$line" | cut -d " " -f 2 | xargs geoiplookup | cut -d: -f 2 | tr -d '\n')
+
+		if grep -q `echo "$line" | cut -d " " -f 2` fail2ban_day.log;then
+			banned="BANNED"
+		else
+			banned=""
+		fi
+
+		printf "      %-18s : %-25s %s\n" "$line" "$location" "$banned"
+	done < failed_ips.log
+
+	echo ""
+else
+	echo ""
+fi
+
+
+##FAIL2BAN SECTION
+# Count number of bans
+fail2ban_bans=$(grep "] Ban" fail2ban_day.log | wc -l)
+
+# Print words
+printf "Fail2Ban blocked %d IP address(es) that attempted to connect too much\n" "$fail2ban_bans"
+
+
+##HTML END STUFF
+if [ "$SENDMAILFLAG" = "true" ]; then
+	echo "</pre>"
+	echo "</body>"
+	echo "</html>"
+fi
+
+##CLEAN UP
+rm -f day.log
+rm -f successful_auths.log
+rm -f successful_ips.log
+rm -f failed_auths.log
+rm -f failed_ips.log
+rm -f fail2ban_day.log
